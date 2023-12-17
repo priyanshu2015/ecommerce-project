57ebc92c3eb2ee7b7dbf4420991af7a1bf31a3ad
unknown <priyanshu.gupta2008@gmail.com>
2023-12-17 11:03:27 +0530

    fixed celery task code and added signals

--- src/notifications/admin.py
+++ src/notifications/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from notifications.models import ScheduledNotification, Notification
 
 # Register your models here.
+admin.site.register(ScheduledNotification)
+admin.site.register(Notification)
